57a06dc57f3a746c5502d5b55129a583a9ee9f94
Juan Moreno <morjua19@gmail.com>
2022-04-21 18:46:05 -0400

    [UPD] getUser

--- src/schemas/Queries/User.ts
+++ src/schemas/Queries/User.ts
@@ -1,10 +1,25 @@
 import { Users } from '../../entities/Users';
 import { UserType } from '../../typeDefs/User';
-import { GraphQLList } from 'graphql';
+import { GraphQLID, GraphQLList } from 'graphql';
 
 export const GET_ALL_USERS = {
   type: new GraphQLList(UserType),
   async resolve() {
     return await Users.find();
+  }
+}
+
+export const GET_USER = {
+  type: UserType,
+  args: {
+    id: {type: GraphQLID}
+  },
+  async resolve(_: any, args: any) {
+    const {id} = args;
+    return await Users.findOne({
+      where: {
+        id
+      }
+    });
   }
 }
