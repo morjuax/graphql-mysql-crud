a99fe9d528df90d6971a62c2bad0053bba62db9f
Juan Moreno <morjua19@gmail.com>
2022-04-21 17:26:59 -0400

    [UPD] create user

--- src/schemas/Mutations/User.ts
+++ src/schemas/Mutations/User.ts
@@ -1,14 +1,29 @@
 import { GraphQLString } from 'graphql';
+import { Users } from '../../entities/Users';
+import { UserType } from '../../typeDefs/User';
+import bcrypt from 'bcryptjs'
 
 export const CREATE_USER = {
-  type: GraphQLString,
+  type: UserType,
   args: {
     name: { type: GraphQLString },
     userName: { type: GraphQLString },
     password: { type: GraphQLString }
   },
-  resolve(_: any, args: any) {
-    console.log(args)
-    return 'User created'
+  async resolve(_: any, args: any) {
+    const {name, userName, password} = args;
+
+    const encryptPassword = await bcrypt.hash(password, 10)
+    const result = await Users.insert({
+      name,
+      userName,
+      password: encryptPassword
+    });
+
+    return {
+      ...args,
+      id: result.identifiers[0].id,
+      password: encryptPassword
+    }
   }
 }
