4704163e779002a4ddf5c3a31eb9f8994c422254
Juan Moreno <morjua19@gmail.com>
2022-04-28 13:07:02 -0400

    [UPD]

--- src/schemas/Mutations/User.ts
+++ src/schemas/Mutations/User.ts
@@ -1,4 +1,4 @@
-import { GraphQLString } from 'graphql';
+import { GraphQLBoolean, GraphQLID, GraphQLString } from 'graphql';
 import { Users } from '../../entities/Users';
 import { UserType } from '../../typeDefs/User';
 import bcrypt from 'bcryptjs'
@@ -25,5 +25,43 @@ export const CREATE_USER = {
       id: result.identifiers[0].id,
       password: encryptPassword
     }
+  }
+}
+
+export const DELETE_USER = {
+  type: GraphQLBoolean,
+  args: {
+    id: {type: GraphQLID}
+  },
+  async resolve(_: any, {id}: any) {
+    const result = await Users.delete(id)
+    return result.affected
+  }
+}
+
+export const UPDATE_USER = {
+  type: GraphQLBoolean,
+  args: {
+    id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    userName: {type: GraphQLString},
+    oldPassword: {type: GraphQLString},
+    newPassword: {type: GraphQLString},
+  },
+  async resolve(_: any, {id, name, userName, oldPassword, newPassword}: any) {
+    const userFound = await Users.findOne({
+      where: {
+        id
+      }
+    })
+
+    if (!userFound) return false;
+
+    const isMatch = await bcrypt.compare(oldPassword, userFound.password);
+    if (!isMatch) return false;
+    const newPasswordHashed = await bcrypt.hash(newPassword, 10);
+    const result = await Users.update({id}, {name, userName, password: newPasswordHashed});
+    return result.affected !== 0;
+
   }
 }
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,6 @@
 import { GraphQLSchema, GraphQLObjectType } from 'graphql'
 import { GREETING } from './Queries/Greeting';
-import { CREATE_USER } from './Mutations/User';
+import { CREATE_USER, DELETE_USER, UPDATE_USER } from './Mutations/User';
 import { GET_ALL_USERS, GET_USER } from './Queries/User';
 
 const RootQuery = new GraphQLObjectType({
@@ -15,7 +15,9 @@ const RootQuery = new GraphQLObjectType({
 const Mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    createUser: CREATE_USER
+    createUser: CREATE_USER,
+    deleteUser: DELETE_USER,
+    updateUser: UPDATE_USER
   }
 })
 
